1330e7a666f9784f2d19ab50eea9ea4f6bf16b36
0xJayPi <jpcampaya@gmail.com>
2022-10-26 19:54:34 +0200

    Created most of the scripting

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,13 +10,13 @@ require("dotenv").config()
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const GOERLI_RPC_URL =
-    process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
 // optional
 
 // API key for Etherscan
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 // API key to make gas convertions to USD
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
@@ -26,7 +26,8 @@ module.exports = {
     networks: {
         hardhat: {
             chainId: 31337,
-            blockConfirmations: 1,
+            forking: { url: MAINNET_RPC_URL },
+            // blockNumber: 1200000, // tests should be done with a fixed block to prevent using different blockchain states between each of them
         },
         localhost: {
             chainId: 31337,
@@ -52,13 +53,21 @@ module.exports = {
     namedAccounts: {
         deployer: {
             default: 0, // here this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+            //1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
         },
         user1: {
             default: 1,
         },
     },
-    solidity: "0.8.7",
+    solidity: {
+        compilers: [
+            { version: "0.8.7" },
+            { version: "0.4.19" },
+            { version: "0.6.12" },
+            { version: "0.6.6" },
+            { version: "0.6.0" },
+        ],
+    },
     mocha: {
         timeout: 2000000, // 2000 seconds
     },
---
+++ scripts/aaveBorrow.js
@@ -0,0 +1,73 @@
+/**  This script runs slower than usual because it's calling the Alchemy API for Mainnet on every run.
+ * Because of being a fork from mainnet.
+ * It just brings the contract data for only the contracts I'm calling*
+ */
+
+const { ethers, getNamedAccounts } = require("hardhat")
+const { getWeth, AMOUNT } = require("../scripts/getWeth")
+
+async function main() {
+    await getWeth()
+    const { deployer } = await getNamedAccounts()
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`Lending Pool address: ${lendingPool.address}`)
+
+    const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer)
+
+    console.log("Depositing...")
+    await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
+    console.log("Deposited!!")
+
+    let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(lendingPool, deployer)
+
+    const daiPrice = await getDaiPrice()
+    const amountDaitToBorrow = availableBorrowsETH.toString() * 0.95 * (1 / daiPrice.toNumber())
+    console.log(`You can borrow ${amountDaitToBorrow} DAI`)
+    const amountDaitToBorrowWei = ethers.utils.parseEther(amountDaitToBorrow.toString())
+}
+
+async function getDaiPrice() {
+    const daiEthPriceFeed = await ethers.getContractAt(
+        "AggregatorV3Interface",
+        "0x773616E4d11A78F511299002da57A0a94577F1f4"
+    )
+    const price = (await daiEthPriceFeed.latestRoundData())[1] // I just want the answer, which is the 2nd element
+    console.log(`The DAI/ETH price is ${price}`)
+    return price
+}
+
+async function getBorrowUserData(lendingPool, account) {
+    const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+        await lendingPool.getUserAccountData(account)
+    console.log(`You have ${totalCollateralETH} worth of ETH deposited`)
+    console.log(`You have ${totalDebtETH} worth of ETH borrowed`)
+    console.log(`You can borrow ${availableBorrowsETH} worth of ETH`)
+    return { availableBorrowsETH, totalDebtETH }
+}
+
+async function getLendingPool(account) {
+    const lendingPoolAddressesProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+        account
+    )
+    const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, account)
+
+    return lendingPool
+}
+
+async function approveErc20(erc20Address, spenderAddress, amountToExpend, account) {
+    const erc20Token = await ethers.getContractAt("IERC20", erc20Address, account)
+    const txResponse = await erc20Token.approve(spenderAddress, amountToExpend)
+    await txResponse.wait(1)
+    console.log("Approved!")
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
