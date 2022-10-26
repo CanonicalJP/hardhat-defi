ac25654bb68e65143005cc8d528eb5e611d2da7e
0xJayPi <jpcampaya@gmail.com>
2022-10-26 17:01:51 +0200

    Initial Commit

---
+++ hardhat.config.js
@@ -0,0 +1,65 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
+// optional
+
+// API key for Etherscan
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
+
+// API key to make gas convertions to USD
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            saveDeployments: true,
+            chainId: 5,
+            blockConfirmations: 6,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+        user1: {
+            default: 1,
+        },
+    },
+    solidity: "0.8.7",
+    mocha: {
+        timeout: 2000000, // 2000 seconds
+    },
+}
---
+++ helper-hardhat-config.js
@@ -0,0 +1,37 @@
+const { ethers } = require("hardhat")
+
+const networkConfig = {
+    default: {
+        name: "hardhat",
+        keepersUpdateInterval: "30",
+    },
+    5: {
+        name: "goerli",
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+        entranceFee: ethers.utils.parseEther("0.0001"),
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+        subscriptionId: "5053",
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+    31337: {
+        name: "hardhat",
+        entranceFee: ethers.utils.parseEther("0.1"),
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // it doesn't really matters the value as we're using the Mock
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+const frontEndContractsFile = "../nextjs-smartcontract-lottery/constants/contractAddresses.json"
+const frontEndAbiFile = "../nextjs-smartcontract-lottery/constants/abi.json"
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+    frontEndContractsFile,
+    frontEndAbiFile,
+}
